79caa5a2fb4672d21c10b51b9e853f8531082a2a
ahmed ghaly <ahmedghaly0767@gmail.com>
2023-04-14 16:54:10 +0200

    Project files have been uploaded

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import 'modules/home_screen.dart';
+
+void main() {
+  runApp(const TokuApp());
+}
+
+class TokuApp extends StatelessWidget {
+  const TokuApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+        appBarTheme: const AppBarTheme(
+          centerTitle: true,
+          elevation: 0,
+          titleTextStyle: TextStyle(
+            fontSize: 23,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
